1515400726b2224b9ad4bdafffb0c7dc23c1ad65
Slava <rtootik@gmail.com>
2017-01-18 22:07:00 +0300

    Initial Commit

---
+++ 05-HomeWork-Notification/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  05-HomeWork-Notification
+//
+//  Created by Slava on 18.01.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 05-HomeWork-Notification/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  05-HomeWork-Notification
+//
+//  Created by Slava on 18.01.17.
+//  Copyright © 2017 Slava. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
